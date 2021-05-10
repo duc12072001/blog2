df0a4f1fba180a9905db7e0d2e46c22538fffd84
duc12072001 <71589748+duc12072001@users.noreply.github.com>
2021-05-10 21:09:35 +0700

    Update footer.php

--- admin/layout/footer.php
+++ admin/layout/footer.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2019 <a href="https://virtuaphp" target="_blank">VirtuaPHP</a>.</strong> All rights
+    <strong>Copyright &copy; <a href="https://virtuaphp" target="_blank">VirtuaPHP</a>.</strong> All rights
     reserved.
   </footer>
