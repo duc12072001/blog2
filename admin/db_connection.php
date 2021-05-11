d6c0ce60776e15f75809b282ec03996fa4ace654
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2021-05-11 10:37:48 +0700

    Update db_connection.php

--- admin/db_connection.php
+++ admin/db_connection.php
@@ -1,4 +1,5 @@
 <?php 
+	date_default_timezone_set("Asia/Ho_Chi_Minh");
 	session_start();
 	$dbconnection = mysqli_connect('remotemysql.com', 'tGlXeaiQC0', 'etC2E1WJct', 'tGlXeaiQC0');
 	mysqli_set_charset($dbconnection, "utf8");
